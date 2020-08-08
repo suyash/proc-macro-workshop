a781734d57235a2c63c57c1886cc900e42b207f2
Suyash <hi@suy.io>
2020-08-08 21:43:19 +0530

    debug: non formatting stuff

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -1,10 +1,35 @@
 extern crate proc_macro;
 
 use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(CustomDebug)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let inp = parse_macro_input!(input as DeriveInput);
 
-    unimplemented!()
+    let ident = &inp.ident;
+    let ans = if let syn::Data::Struct(ref data) = &inp.data {
+        if let syn::Fields::Named(syn::FieldsNamed { ref named, .. }) = data.fields {
+            let names: Vec<&Option<syn::Ident>> = named.iter().map(|v| &v.ident).collect();
+
+            quote! {
+                struct X {
+                    #named
+                }
+
+                impl ::std::fmt::Debug for #ident {
+                    fn fmt(&self, fmt: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
+                        fmt.debug_struct(stringify!(#ident))#(.field(stringify!(#names), &self.#names))*.finish()
+                    }
+                }
+            }
+        } else {
+            unimplemented!();
+        }
+    } else {
+        unimplemented!();
+    };
+
+    TokenStream::from(ans)
 }
