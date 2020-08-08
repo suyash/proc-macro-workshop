3dba652a78e929098ebae9c4f27d81f6be4999a8
Suyash <hi@suy.io>
2020-08-08 20:05:19 +0530

    builder: get nightly only working for 8

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,13 +1,28 @@
 use proc_macro::TokenStream;
 use quote::{format_ident, quote};
+use syn::spanned::Spanned;
 use syn::{parse_macro_input, DeriveInput};
 
-#[proc_macro_derive(Builder)]
+fn internal_type<'a>(args: &'a syn::PathArguments) -> Option<&'a syn::Type> {
+    if let syn::PathArguments::AngleBracketed(syn::AngleBracketedGenericArguments {
+        ref args,
+        ..
+    }) = args
+    {
+        let arg = args.first().unwrap();
+        if let syn::GenericArgument::Type(ref ity) = arg {
+            return Some(ity);
+        }
+    }
+
+    None
+}
+
+#[proc_macro_derive(Builder, attributes(builder))]
 pub fn derive(input: TokenStream) -> TokenStream {
     let inp = parse_macro_input!(input as DeriveInput);
     let name = inp.ident;
     let buildername = format_ident!("{}Builder", name);
-    
     let fields = if let syn::Data::Struct(syn::DataStruct {
         fields: syn::Fields::Named(syn::FieldsNamed { named, .. }),
         ..
@@ -18,34 +33,181 @@ pub fn derive(input: TokenStream) -> TokenStream {
         unimplemented!();
     };
 
-    let fnames = fields.iter().map(|f| &f.ident).collect::<Vec<&Option<syn::Ident>>>();
-    let ftypes = fields.iter().map(|f| &f.ty).collect::<Vec<&syn::Type>>();
+    let builder_fields = fields.iter().map(|f| {
+        let ident = &f.ident;
+        let ty = &f.ty;
+
+        if let syn::Type::Path(syn::TypePath {
+            path: syn::Path { ref segments, .. },
+            ..
+        }) = ty
+        {
+            if segments.len() == 1 {
+                let segment = segments.first().unwrap();
+                let tident = &segment.ident;
+                let args = &segment.arguments;
+                if tident == "Option" {
+                    if let syn::PathArguments::AngleBracketed(
+                        syn::AngleBracketedGenericArguments { ref args, .. },
+                    ) = args
+                    {
+                        let arg = args.first().unwrap();
+                        if let syn::GenericArgument::Type(ref ity) = arg {
+                            return quote! { #ident: Option<#ity> };
+                        } else {
+                            unimplemented!();
+                        }
+                    } else {
+                        unimplemented!();
+                    }
+                }
+            }
+        }
+
+        quote! { #ident: Option<#ty> }
+    });
+
+    let fns = fields.iter().map(|f| {
+        let ident = &f.ident;
+        let ty = &f.ty;
+        let attrs = &f.attrs;
+
+        if let syn::Type::Path(syn::TypePath {
+            path: syn::Path { ref segments, .. },
+            ..
+        }) = ty
+        {
+            if segments.len() == 1 {
+                let tident = &segments[0].ident;
+                let args = &segments[0].arguments;
+                if tident == "Option" {
+                    let ity = internal_type(args).unwrap();
+                    return quote! {
+                        fn #ident(&mut self, #ident: #ity) -> &mut Self {
+                            self.#ident = Some(#ident);
+                            self
+                        }
+                    };
+                }
+
+                if tident == "Vec" && attrs.len() == 1 {
+                    let att = attrs.first().unwrap();
+                    let meta = att.parse_meta().unwrap();
+                    if let syn::Meta::List(ref ml) = meta {
+                        let nested = &ml.nested;
+                        if nested.len() == 1 {
+                            if let syn::NestedMeta::Meta(syn::Meta::NameValue(ref nv)) = nested[0] {
+                                let path = &nv.path;
+                                let litstr = match &nv.lit {
+                                    syn::Lit::Str(ref s) => s,
+                                    _ => unimplemented!(),
+                                };
+                                if path.segments.len() == 1 {
+                                    let segident = &(path.segments[0]).ident;
+                                    if segident == "each" {
+                                        let name = litstr.value();
+                                        let name = name.as_str();
+                                        let iident = syn::Ident::new(name, litstr.span());
+                                        let ity = internal_type(args).unwrap();
+                                        return quote! {
+                                            fn #iident(&mut self, #iident: #ity) -> &mut Self {
+                                                self.#ident = match self.#ident.take() {
+                                                    None => Some(vec![#iident]),
+                                                    Some(mut v) => {
+                                                        v.push(#iident);
+                                                        Some(v)
+                                                    }
+                                                };
+                                                self
+                                            }
+                                        };
+                                    } else {
+                                        let err = syn::Error::new(
+                                            ml.path.span().join(ml.paren_token.span).unwrap(),
+                                            "expected `builder(each = \"...\")`",
+                                        );
+                                        return err.to_compile_error();
+                                    }
+                                } else {
+                                    unimplemented!();
+                                }
+                            } else {
+                                unimplemented!();
+                            }
+                        }
+                    } else {
+                        unimplemented!();
+                    }
+                }
+            }
+        }
+
+        quote! {
+            fn #ident(&mut self, #ident: #ty) -> &mut Self {
+                self.#ident = Some(#ident);
+                self
+            }
+        }
+    });
+
+    let build_args = fields.iter().map(|f| {
+        let ident = &f.ident;
+        let ty = &f.ty;
+
+        if let syn::Type::Path(syn::TypePath {
+            path: syn::Path { ref segments, .. },
+            ..
+        }) = ty
+        {
+            if segments.len() == 1 {
+                let segment = segments.first().unwrap();
+                let tident = &segment.ident;
+                if tident == "Option" {
+                    return quote! {
+                        #ident: self.#ident.take()
+                    };
+                }
+
+                if tident == "Vec" && f.attrs.len() == 1 {
+                    return quote! {
+                        #ident: match (self.#ident.take()) {
+                            None => vec![],
+                            Some(v) => v
+                        }
+                    };
+                }
+            }
+        }
+
+        quote! {
+            #ident: self.#ident.take().ok_or("Fail")?
+        }
+    });
+
+    let build_init = fields.iter().map(|f| {
+        let ident = &f.ident;
+        quote! { #ident: None }
+    });
 
     let expanded = quote! {
         impl #name {
             pub fn builder() -> #buildername {
                 #buildername {
-                    executable: None,
-                    args: None,
-                    env: None,
-                    current_dir: None,
+                    #(#build_init),*
                 }
             }
         }
 
         pub struct #buildername {
-            #(#fnames: Option<#ftypes>),*
+            #(#builder_fields),*
         }
 
         impl #buildername {
-            #(fn #fnames(&mut self, #fnames: #ftypes) -> &mut Self {
-                self.#fnames = Some(#fnames);
-                self
-            })*
+            #(#fns)*
 
             pub fn build(&mut self) -> Result<#name, Box<dyn ::std::error::Error>> {
                 Ok(#name {
-                    #(#fnames: self.#fnames.take().ok_or("Fail")?),*
+                    #(#build_args),*
                 })
             }
         }
