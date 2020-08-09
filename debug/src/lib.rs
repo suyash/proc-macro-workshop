0a3c0b4298e4ba6aaeea1736c3b863eb20303289
Suyash <hi@suy.io>
2020-08-09 11:17:09 +0530

    debug: 7

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -2,13 +2,12 @@ extern crate proc_macro;
 
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, DeriveInput, parse_quote};
+use syn::{parse_macro_input, parse_quote, punctuated::Punctuated, spanned::Spanned, DeriveInput};
 
 #[proc_macro_derive(CustomDebug, attributes(debug))]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let inp = parse_macro_input!(input as DeriveInput);
+    let mut inp = parse_macro_input!(input as DeriveInput);
 
-    let ident = &inp.ident;
     let ans = if let syn::Data::Struct(ref data) = &inp.data {
         if let syn::Fields::Named(syn::FieldsNamed { ref named, .. }) = data.fields {
             let names: Vec<&Option<syn::Ident>> = named.iter().map(|v| &v.ident).collect();
@@ -25,8 +24,10 @@ pub fn derive(input: TokenStream) -> TokenStream {
                 "{:?}".to_owned()
             });
 
-            let generics = add_trait_bounds(inp.generics);
-            let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
+            add_trait_bounds(&mut inp.generics, named);
+
+            let (impl_generics, ty_generics, where_clause) = inp.generics.split_for_impl();
+            let ident = &inp.ident;
 
             quote! {
                 impl #impl_generics ::std::fmt::Debug for #ident #ty_generics #where_clause {
@@ -45,12 +46,109 @@ pub fn derive(input: TokenStream) -> TokenStream {
     TokenStream::from(ans)
 }
 
-fn add_trait_bounds(mut generics: syn::Generics) -> syn::Generics {
+fn add_trait_bounds(
+    generics: &mut syn::Generics,
+    fields: &Punctuated<syn::Field, syn::token::Comma>,
+) {
+    let where_token = syn::token::Where {
+        span: generics.span(),
+    };
+
     for param in &mut generics.params {
         if let syn::GenericParam::Type(ref mut tp) = param {
-            tp.bounds.push(parse_quote!(::std::fmt::Debug));
+            if check(&tp.ident, fields) {
+                let mut found = false;
+
+                for field in fields {
+                    let ty = &field.ty;
+                    if let syn::Type::Path(syn::TypePath {
+                        path: syn::Path { ref segments, .. },
+                        ..
+                    }) = ty
+                    {
+                        if segments.len() == 1 {
+                            if let syn::PathSegment {
+                                arguments:
+                                    syn::PathArguments::AngleBracketed(
+                                        syn::AngleBracketedGenericArguments { ref args, .. },
+                                    ),
+                                ..
+                            } = &segments[0]
+                            {
+                                let arg = &args[0];
+                                if let syn::GenericArgument::Type(ref ty) = arg {
+                                    if let syn::Type::Path(syn::TypePath {
+                                        path: syn::Path { ref segments, .. },
+                                        ..
+                                    }) = ty
+                                    {
+                                        if segments.len() > 1 {
+                                            let predicates: Punctuated<
+                                                syn::WherePredicate,
+                                                syn::token::Comma,
+                                            > = parse_quote! {
+                                                #ty: ::std::fmt::Debug
+                                            };
+
+                                            // TODO: this only allows one clause, figure out doing more
+
+                                            generics.where_clause = Some(syn::WhereClause {
+                                                where_token,
+                                                predicates,
+                                            });
+
+                                            found = true;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+
+                if !found {
+                    tp.bounds.push(parse_quote!(::std::fmt::Debug));
+                }
+            }
+        }
+    }
+}
+
+fn check(ident: &syn::Ident, fields: &Punctuated<syn::Field, syn::token::Comma>) -> bool {
+    for field in fields {
+        let ty = &field.ty;
+        if let syn::Type::Path(syn::TypePath {
+            path: syn::Path { ref segments, .. },
+            ..
+        }) = ty
+        {
+            if segments.len() == 1 {
+                let segment = &segments[0];
+                let sident = &segment.ident;
+                if let syn::PathArguments::AngleBracketed(syn::AngleBracketedGenericArguments {
+                    ref args,
+                    ..
+                }) = &segment.arguments
+                {
+                    if args.len() == 1 {
+                        let arg = &args[0];
+                        if let syn::GenericArgument::Type(syn::Type::Path(syn::TypePath {
+                            path: syn::Path { ref segments, .. },
+                            ..
+                        })) = arg
+                        {
+                            if segments.len() == 1 {
+                                let segment = &segments[0];
+                                if &segment.ident == ident && sident == "PhantomData" {
+                                    return false;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 
-    generics
+    true
 }
