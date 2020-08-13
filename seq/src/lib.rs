0018e99ccd0c3abc1f4f10c5376d86904c793792
Suyash <hi@suy.io>
2020-08-13 20:21:41 +0530

    seq: expansion

--- seq/src/lib.rs
+++ seq/src/lib.rs
@@ -1,10 +1,41 @@
 extern crate proc_macro;
 
 use proc_macro::TokenStream;
+use proc_macro2::Group;
+use syn::{Result, Ident, Token, LitInt, parse_macro_input, parse::{ParseStream, Parse}};
+use quote::quote;
+
+#[derive(Debug)]
+struct SeqInput {
+    name: Ident,
+    start: LitInt,
+    end: LitInt,
+    body: Group,
+}
+
+impl Parse for SeqInput {
+    fn parse(input: ParseStream) -> Result<Self> {
+        let name: Ident = input.parse()?;
+        let _: Token![in] = input.parse()?;
+        let start: LitInt = input.parse()?;
+        let _: Token![..] = input.parse()?;
+        let end: LitInt = input.parse()?;
+        let body: Group = input.parse()?;
+
+        Ok(SeqInput{
+            name,
+            start, 
+            end,
+            body,
+        })
+    }
+}
 
 #[proc_macro]
 pub fn seq(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let _ = parse_macro_input!(input as SeqInput);
 
-    unimplemented!()
+    let ans = quote!{};
+
+    TokenStream::from(ans)
 }
