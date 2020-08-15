598b9b3684e50bdc05431dcc3b09dd93699c8ffc
Suyash <hi@suy.io>
2020-08-15 17:08:00 +0530

    seq: impl repeat

--- seq/src/lib.rs
+++ seq/src/lib.rs
@@ -1,7 +1,7 @@
 extern crate proc_macro;
 
 use proc_macro::TokenStream;
-use proc_macro2::{Group, Literal, TokenStream as TokenStream2, TokenTree as TokenTree2};
+use proc_macro2::{Group, Literal, Delimiter, TokenStream as TokenStream2, TokenTree as TokenTree2};
 use syn::{
     parse::{Parse, ParseStream},
     parse_macro_input, Ident, LitInt, Result, Token,
@@ -10,8 +10,8 @@ use syn::{
 #[derive(Debug)]
 struct SeqInput {
     name: Ident,
-    start: LitInt,
-    end: LitInt,
+    start: u64,
+    end: u64,
     body: Group,
 }
 
@@ -24,6 +24,9 @@ impl Parse for SeqInput {
         let end: LitInt = input.parse()?;
         let body: Group = input.parse()?;
 
+        let start = start.base10_parse::<u64>().unwrap();
+        let end = end.base10_parse::<u64>().unwrap();
+
         Ok(SeqInput {
             name,
             start,
@@ -34,6 +37,83 @@ impl Parse for SeqInput {
 }
 
 impl SeqInput {
+    fn rep_block(&self, group: &Group) -> (TokenStream2, bool) {
+        let mut iter = group.stream().into_iter();
+
+        let mut last1 = None;
+        let mut last2 = None;
+
+        let mut repeated = false;
+
+        let mut v = Vec::new();
+
+        loop {
+            match iter.next() {
+                None => break,
+                Some(tt) => {
+                    match &tt {
+                        TokenTree2::Group(ref g) => {
+                            let (pg, pr) = self.rep_block(g);
+                            repeated = repeated || pr;
+                            let ng = Group::new(g.delimiter(), pg);
+
+                            if last2.is_some() {
+                                v.push(last2.unwrap());
+                            }
+
+                            last2 = last1;
+                            last1 = Some(TokenTree2::Group(ng));
+                            continue;
+                        }
+
+                        TokenTree2::Punct(ref punct) if punct.as_char() == '*' => {
+                            if last2.is_some() && last1.is_some() {
+                                match (last2.as_ref().unwrap(), last1.as_ref().unwrap()) {
+                                    (TokenTree2::Punct(ref punct2), TokenTree2::Group(ref group)) if punct2.as_char() == '#' && group.delimiter() == Delimiter::Parenthesis => {
+                                        repeated = true;
+
+                                        let pt = (self.start..self.end)
+                                            .map(|i| self.expand(group.stream(), i))
+                                            .collect::<TokenStream2>();
+
+                                        v.pop();
+                                        v.pop();
+                                        v.push(TokenTree2::Group(Group::new(Delimiter::None, pt)));
+
+                                        last2 = None;
+                                        last1 = None;
+
+                                        continue;
+                                    }
+                                    _ => {}
+                                }
+                            }
+                        }
+
+                        _ => {}
+                    }
+
+                    if last2.is_some() {
+                        v.push(last2.unwrap());
+                    }
+
+                    last2 = last1;
+                    last1 = Some(tt);
+                }
+            }
+        }
+
+        if last2.is_some() {
+            v.push(last2.unwrap());
+        }
+
+        if last1.is_some() {
+            v.push(last1.unwrap());
+        }
+
+        (v.into_iter().collect(), repeated)
+    }
+
     fn expand(&self, stream: TokenStream2, index: u64) -> TokenStream2 {
         let mut iter = stream.into_iter();
         let mut v = Vec::new();
@@ -116,12 +196,14 @@ impl SeqInput {
 
 impl Into<TokenStream2> for SeqInput {
     fn into(self) -> TokenStream2 {
-        let start = self.start.base10_parse::<u64>().unwrap();
-        let end = self.end.base10_parse::<u64>().unwrap();
-
-        (start..end)
-            .map(|i| self.expand(self.body.stream(), i))
-            .collect::<TokenStream2>()
+        let (ts, repeated) = self.rep_block(&self.body);
+        if repeated {
+            ts
+        } else {
+            (self.start..self.end)
+                .map(|i| self.expand((&self.body).stream(), i))
+                .collect::<TokenStream2>()
+        }
     }
 }
 
