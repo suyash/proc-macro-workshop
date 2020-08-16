344b80a69f6c4dff78f0d05c0cd70c29e6e40678
Suyash <hi@suy.io>
2020-08-16 08:52:19 +0530

    sorted: 4

--- sorted/src/lib.rs
+++ sorted/src/lib.rs
@@ -1,8 +1,6 @@
-extern crate proc_macro;
-
 use proc_macro::TokenStream;
 
-use proc_macro2::{TokenStream as TokenStream2};
+use proc_macro2::TokenStream as TokenStream2;
 use quote::quote;
 use syn::{parse_macro_input, Error, Item};
 
@@ -12,6 +10,29 @@ pub fn sorted(args: TokenStream, input: TokenStream) -> TokenStream {
 
     match item {
         Item::Enum(itemenum) => {
+            let variants = &itemenum.variants;
+
+            if !variants.is_empty() {
+                let mut v = vec![variants.first().unwrap()];
+                for val in variants.iter().skip(1) {
+                    if &v.last().unwrap().ident > &val.ident {
+                        for vv in v.iter() {
+                            if &vv.ident > &val.ident {
+                                let msg = format!("{} should sort before {}", &val.ident, &vv.ident);
+                                let err = Error::new_spanned(&val.ident, msg.as_str());
+
+                                let base = quote!{ #itemenum };
+                                let curr = err.to_compile_error();
+
+                                return vec![base, curr].into_iter().collect::<TokenStream2>().into();
+                            }
+                        }
+                    }
+
+                    v.push(val);
+                }
+            }
+
             let stream = quote! { #itemenum };
             TokenStream::from(stream)
         }
