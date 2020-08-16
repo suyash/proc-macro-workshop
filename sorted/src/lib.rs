4756182eb4efea8b925c33bafd8e686e83b3d497
Suyash <hi@suy.io>
2020-08-16 10:33:41 +0530

    sorted: 6

--- sorted/src/lib.rs
+++ sorted/src/lib.rs
@@ -2,7 +2,7 @@ use proc_macro::TokenStream;
 
 use proc_macro2::TokenStream as TokenStream2;
 use quote::quote;
-use syn::{parse_macro_input, Error, Item};
+use syn::{parse_macro_input, Error, Item, ItemFn, ExprMatch, Pat, visit_mut::{self, VisitMut}};
 
 #[proc_macro_attribute]
 pub fn sorted(args: TokenStream, input: TokenStream) -> TokenStream {
@@ -43,5 +43,75 @@ pub fn sorted(args: TokenStream, input: TokenStream) -> TokenStream {
             );
             err.to_compile_error().into()
         }
+    }
+}
+
+struct Check {
+    err: Option<Error>
+}
+
+impl VisitMut for Check {
+    fn visit_expr_match_mut(&mut self, node: &mut ExprMatch) {
+        if node.attrs.len() == 1 {
+            node.attrs.clear();
+
+            // TODO: check that the attribute is sorted
+
+            if node.arms.len() > 1 {
+                let mut v = vec![&node.arms[0].pat];
+
+                for arm in node.arms.iter().skip(1) {
+                    let pat = &arm.pat;
+
+                    match (v.last().unwrap(), pat) {
+                        (Pat::TupleStruct(ref first), Pat::TupleStruct(ref second)) => {
+                            println!("{:#?}", second);
+
+                            let s1 = &first.path.segments;
+                            let s2 = &second.path.segments;
+
+                            let mut cs1 = "".to_string();
+                            let mut cs2 = "".to_string();
+
+                            for (i1, i2) in s1.iter().zip(s2.iter()) {
+                                let ident1 = &i1.ident;
+                                let ident2 = &i2.ident;
+
+                                cs1 = format!("{}::{}", cs1, ident1);
+                                cs2 = format!("{}::{}", cs2, ident2);
+
+                                if ident1 > ident2 {
+                                    let msg = format!("{} should sort before {}", &cs2[2..], &cs1[2..]);
+                                    let err = Error::new_spanned(&s2, msg.as_str());
+                                    self.err = Some(err);
+                                }
+                            }
+                        },
+                        _ => {}
+                    }
+
+                    v.push(&pat);
+                }
+            }
+        }
+        visit_mut::visit_expr_match_mut(self, node);
+    }
+}
+
+#[proc_macro_attribute]
+pub fn check(_args: TokenStream, input: TokenStream) -> TokenStream {
+    let mut itemfn = parse_macro_input!(input as ItemFn);
+
+    let mut checker = Check { err: None };
+    checker.visit_item_fn_mut(&mut itemfn);
+
+    let stream = quote!{#itemfn};
+
+    match checker.err {
+        Some(err) => {
+            let stream: TokenStream2 = vec![stream, err.to_compile_error()].into_iter().collect();
+            stream.into()
+        },
+        None => stream.into(),
     }
 }
