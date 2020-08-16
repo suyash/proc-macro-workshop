b86d4edb04ef0754eeffc5d8f6441fc8d216e068
Suyash <hi@suy.io>
2020-08-16 08:13:52 +0530

    sorted: 2

--- sorted/src/lib.rs
+++ sorted/src/lib.rs
@@ -2,10 +2,25 @@ extern crate proc_macro;
 
 use proc_macro::TokenStream;
 
+use proc_macro2::{TokenStream as TokenStream2};
+use quote::quote;
+use syn::{parse_macro_input, Error, Item};
+
 #[proc_macro_attribute]
 pub fn sorted(args: TokenStream, input: TokenStream) -> TokenStream {
-    let _ = args;
-    let _ = input;
+    let item = parse_macro_input!(input as Item);
 
-    unimplemented!()
+    match item {
+        Item::Enum(itemenum) => {
+            let stream = quote! { #itemenum };
+            TokenStream::from(stream)
+        }
+        _ => {
+            let err = Error::new_spanned(
+                TokenStream2::from(args),
+                "expected enum or match expression",
+            );
+            err.to_compile_error().into()
+        }
+    }
 }
